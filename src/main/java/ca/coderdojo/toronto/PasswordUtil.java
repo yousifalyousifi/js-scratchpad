0691f7fe3bea45c6a97cf129087501ddd1a6212b
Yousif Al-Yousifi <yousifucv@hotmail.com>
2017-02-27 22:13:19 -0500

    Added password hashing and checking

---
+++ src/main/java/ca/coderdojo/toronto/PasswordUtil.java
@@ -0,0 +1,34 @@
+package ca.coderdojo.toronto;
+
+import java.security.SecureRandom;
+import java.util.UUID;
+
+import com.lambdaworks.crypto.SCryptUtil;
+
+public class PasswordUtil {
+	
+	public static String hashPassword(String password) {
+		return SCryptUtil.scrypt(password, (int)Math.pow(2, 14), 8, 1);
+	}
+	
+	public static boolean check(String password, String hashed) {
+		return SCryptUtil.check(password, hashed);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+		System.out.println(hashPassword(UUID.randomUUID().toString()));
+	}
+
+}
